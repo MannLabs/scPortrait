4cc77e59bee38423a26e8fef3dd0d78fc3cd83b3
Sophia Mädler <15019107+sophiamaedler@users.noreply.github.com>
2024-02-01 22:58:15 +0100

    update combine_datasets_balanced to utilize seed

--- src/sparcscore/ml/utils.py
+++ src/sparcscore/ml/utils.py
@@ -1,9 +1,9 @@
 from scipy.sparse import csr_matrix
 import numpy as np
 import torch
-from math import floor
+from math import floor  
 
-def combine_datasets_balanced(list_of_datasets, class_labels, train_per_class, val_per_class, test_per_class,):
+def combine_datasets_balanced(list_of_datasets, class_labels, train_per_class, val_per_class, test_per_class, seed = None):
     """
     Combine multiple datasets to create a single balanced dataset with a specified number of samples per class for train, validation, and test set.
     A balanced dataset means that from each label source an equal number of data instances are used.
@@ -30,7 +30,6 @@ def combine_datasets_balanced(list_of_datasets, class_labels, train_per_class, v
     test : torch.utils.data.Dataset
         Combined test dataset with balanced samples per class.
     """
-    
     elements = [len(el) for el in list_of_datasets]
     rows = np.arange(len(list_of_datasets))
     
@@ -45,22 +44,58 @@ def combine_datasets_balanced(list_of_datasets, class_labels, train_per_class, v
     test_dataset = []
     val_dataset = []
     
-    for dataset, label, fraction in zip(list_of_datasets, class_labels, dataset_fraction):
-        print(dataset, label, fraction)
-        train_size = floor(train_per_class*fraction)
-        test_size = floor(test_per_class*fraction)
-        val_size = floor(val_per_class*fraction)
-        
-        residual_size = len(dataset) - train_size - test_size - val_size
-        
-        if(residual_size < 0):
-            raise ValueError(f"Dataset with length {len(dataset)} is to small to be split into test set of size {test_size} and train set of size {train_size} and validation set of size {val_size}. Use a smaller test and trainset.")
-        
-        train, test, val, _ = torch.utils.data.random_split(dataset, [train_size, test_size, val_size, residual_size])
-        train_dataset.append(train)
-        test_dataset.append(test)
-        val_dataset.append(val)
-    
+    #check to make sure we have more than one occurance of a dataset (otherwise it will throw an error)
+    if np.sum(pd.Series(class_labels).value_counts() > 1) == 0:
+        for dataset, label, fraction in zip(list_of_datasets, class_labels, dataset_fraction):
+            print(dataset, label, 1)
+            train_size = floor(train_per_class)
+            test_size = floor(test_per_class)
+            val_size = floor(val_per_class)
+            
+            residual_size = len(dataset) - train_size - test_size - val_size
+            
+            if(residual_size < 0):
+                raise ValueError(f"Dataset with length {len(dataset)} is to small to be split into test set of size {test_size} and train set of size {train_size} and validation set of size {val_size}. Use a smaller test and trainset.")
+            
+            if seed is not None:
+                print(f"Using seeded generator with seed {seed} to split dataset")
+                gen = torch.Generator()
+                gen.manual_seed(seed)
+                train, test, val, _ = torch.utils.data.random_split(dataset, [train_size, test_size, val_size, residual_size], generator=gen)
+            else:
+                train, test, val, _ = torch.utils.data.random_split(dataset, [train_size, test_size, val_size, residual_size])
+            train_dataset.append(train)
+            test_dataset.append(test)
+            val_dataset.append(val)
+    else: 
+        for dataset, label, fraction in zip(list_of_datasets, class_labels, dataset_fraction):
+            # train_size = floor(train_per_class * fraction)
+            # test_size = floor(test_per_class * fraction)
+            # val_size = floor(val_per_class * fraction)
+            train_size = int(np.round(train_per_class * fraction))
+            test_size = int(np.round(test_per_class * fraction))
+            val_size = int(np.round(val_per_class * fraction))
+            
+            residual_size = len(dataset) - train_size - test_size - val_size
+            
+            if residual_size < 0:
+                raise ValueError(
+                    f"Dataset with length {len(dataset)} is too small to be split into test set of size {test_size}, "
+                    f"train set of size {train_size}, and validation set of size {val_size}. "
+                    f"Use a smaller test and trainset."
+                )
+            if seed is not None:
+                print(f"Using seeded generator with seed {seed} to split dataset")
+                gen = torch.Generator()
+                gen.manual_seed(seed)
+                train, test, val, _ = torch.utils.data.random_split(dataset, [train_size, test_size, val_size, residual_size], generator=gen)
+            else:
+                train, test, val, _ = torch.utils.data.random_split(dataset, [train_size, test_size, val_size, residual_size])
+
+            train_dataset.append(train)
+            test_dataset.append(test)
+            val_dataset.append(val)
+  
     # Convert the combined datasets into torch.utils.data.Dataset objects
     train_dataset = torch.utils.data.ConcatDataset(train_dataset)
     test_dataset = torch.utils.data.ConcatDataset(test_dataset)
