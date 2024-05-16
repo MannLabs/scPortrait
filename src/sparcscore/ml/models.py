d7f0756504a4c9d7ad892d4ea58d0c7c11e4fd08
namsaraeva <a.namsaraeva@tum.de>
2024-05-16 13:56:17 +0200

    printmsg

--- src/sparcscore/ml/models.py
+++ src/sparcscore/ml/models.py
@@ -172,9 +172,7 @@ class VGG2_regression(VGGBase):
                 ):
         
         super(VGG2_regression, self).__init__()
-
-        self.norm = nn.BatchNorm2d(in_channels)
-
+        self.norm = nn.BatchNorm2d(in_channels) 
         self.features = self.make_layers(self.cfgs[cfg], in_channels)
         self.classifier = self.make_layers_MLP(self.cfgs_MLP[cfg_MLP], self.cfgs[cfg], regression=True) # regression is set to True to make the final layer a single output
         
@@ -183,6 +181,7 @@ class VGG2_regression(VGGBase):
         return model
     
     def forward(self, x):
+        print(x.shape)
         x = self.norm(x)
         x = self.features(x)
 
