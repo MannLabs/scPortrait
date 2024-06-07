4c1c16b66234e37cc5326d2ea7290605c335922c
namsaraeva <a.namsaraeva@tum.de>
2024-06-07 14:36:14 +0200

    sort list 2nd try

--- src/sparcscore/ml/datasets.py
+++ src/sparcscore/ml/datasets.py
@@ -341,7 +341,8 @@ class HDF5SingleCellDatasetRegressionSubset(Dataset):
         
         self.dir_list = dir_list 
         self.target_col = target_col
-        self.index_list = index_list.sort() # list of indices to select
+        index_list = index_list.sort()
+        self.index_list = index_list
         self.hours = hours
         self.root_dir = root_dir 
         self.transform = transform 
