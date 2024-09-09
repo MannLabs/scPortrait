383d541a70972ee2cdc525057ff81f3f95289003
namsaraeva <a.namsaraeva@tum.de>
2024-09-09 14:52:32 +0200

    add template for image examples

---
+++ src/scportrait/data/_datasets.py
@@ -0,0 +1,22 @@
+from scportrait.data._dataloader import _download
+from pathlib import Path
+import os
+
+def dataset_1():
+    pass
+
+def dataset_2():
+    pass
+
+def dataset_3():
+    pass
+
+def dataset_4():
+    pass
+
+def dataset_5():
+    pass
+
+def dataset_6():
+    pass
+
