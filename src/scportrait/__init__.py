8547d0fc777a8659279acbda3a81eef62b2a2a5b
Sophia Mädler <15019107+sophiamaedler@users.noreply.github.com>
2025-01-27 15:19:14 +0100

    silences dask warning
    addresses #139

--- src/scportrait/__init__.py
+++ src/scportrait/__init__.py
@@ -1,7 +1,13 @@
 """Top-level package for scPortrait"""
 
+# silence warnings
+import warnings
+
 from scportrait import io
 from scportrait import pipeline as pipeline
 from scportrait import plotting as pl
 from scportrait import processing as pp
 from scportrait import tools as tl
+
+# silence warning from spatialdata resulting in an older dask version see #139
+warnings.filterwarnings("ignore", message="ignoring keyword argument 'read_only'")
