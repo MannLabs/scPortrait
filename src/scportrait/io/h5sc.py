415e79d8a5cb00c6c5a6fe73bd5aa1af37322a80
Sophia Mädler <15019107+sophiamaedler@users.noreply.github.com>
2025-03-18 12:44:40 +0100

    [FIX] ensure that resulting h5sc anndata is not backed
    h5sc does not contain X. backed anndata's require an X as the backing only refers to the X slot

--- src/scportrait/io/h5sc.py
+++ src/scportrait/io/h5sc.py
@@ -8,7 +8,7 @@ from anndata._io.h5ad import _clean_uns, _read_raw, read_dataframe, read_elem
 from scportrait.pipeline._utils.constants import DEFAULT_NAME_SINGLE_CELL_IMAGES, IMAGE_DATACONTAINER_NAME
 
 
-def read_h5sc(filename: str | Path, mode: Literal["r", "r+"] = "r") -> AnnData:
+def read_h5sc(filename: str | Path) -> AnnData:
     """Read scportrait's single-cell image dataset format.
 
     Args:
@@ -18,8 +18,8 @@ def read_h5sc(filename: str | Path, mode: Literal["r", "r+"] = "r") -> AnnData:
     Returns:
         An AnnData object with obsm["single_cell_images"] containing a memory-backed array of the single-cell images.
     """
-
-    d = {"filename": filename, "filemode": mode}
+    mode = "r"  # hard code as there is no benefit from letting the user set this manually as writing back and changing values is currently not supported
+    d = {}
 
     # connect to h5py file
     f = h5py.File(filename, mode)
@@ -37,7 +37,6 @@ def read_h5sc(filename: str | Path, mode: Literal["r", "r+"] = "r") -> AnnData:
     d.update({k: read_elem(f[k]) for k in attributes if k in f})
 
     d["raw"] = _read_raw(f, attrs={"var", "varm"})
-
     adata = AnnData(**d)
 
     # Backwards compat to <0.7
@@ -45,5 +44,4 @@ def read_h5sc(filename: str | Path, mode: Literal["r", "r+"] = "r") -> AnnData:
         _clean_uns(adata)
 
     adata.obsm[DEFAULT_NAME_SINGLE_CELL_IMAGES] = f.get(IMAGE_DATACONTAINER_NAME)
-
     return adata
