35fc3a3672e38eb287161c67ea8db483d408c25e
Sophia Mädler <15019107+sophiamaedler@users.noreply.github.com>
2025-03-23 15:24:10 +0100

    fix pre-commit hooks

--- src/scportrait/tools/spdata/__init__.py
+++ src/scportrait/tools/spdata/__init__.py
@@ -1,8 +1,7 @@
-from . import write
-from . import processing
-from . import processing as pp
-
 # has to be done at the end, after everything has been imported
 import sys
-sys.modules.update({f"{__name__}.{m}": globals()[m] for m in ["pp"]})
 
+from . import processing, write
+from . import processing as pp
+
+sys.modules.update({f"{__name__}.{m}": globals()[m] for m in ["pp"]})
