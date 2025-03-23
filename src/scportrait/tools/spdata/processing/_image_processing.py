63a00034e8055e8117cd8940cb4c61385226edf5
Sophia Mädler <15019107+sophiamaedler@users.noreply.github.com>
2025-03-23 13:58:22 +0100

    first implementation of rescaling function

---
+++ src/scportrait/tools/spdata/processing/_image_processing.py
@@ -0,0 +1,66 @@
+from spatialdata.transformations import get_transformation
+import dask.array as da
+import numpy as np
+import xarray
+
+from scportrait.tools.spdata.write import image as write_image
+
+def _rescale_image(img: da.Array, lower_quantiles:np.ndarray, IPRs:np.ndarray, dtype:np.dtype = np.uint16) -> da.Array:
+    """Helper function to apply rescaling to image
+
+    Args:
+        img: Image to be rescaled
+        lower_quantiles: Lower quantiles for each scale
+        IPRs: Inter-percentile ranges for each scale
+        dtype: Data type of image
+
+    Returns:
+        Rescaled image with the same dtype as the input image.
+    """
+    return(((img - lower_quantiles[:, None, None]) / IPRs[:, None, None]) * np.iinfo(dtype).max).astype(dtype)
+
+
+def percentile_normalize_image(sdata,
+                               image_name: str,
+                               scale_factors: list[int] | None = None,
+                               lower_percentile: int = 1,
+                               upper_percentile: int = 99):
+    if image_name not in sdata:
+        raise ValueError(f"Image {image_name} not found in sdata")
+    image = sdata[image_name]
+
+    if isinstance(image, xarray.DataTree):
+        image = image.get("scale0").image
+        if scale_factors is None:
+            # get scale factors from existing object
+            # currently hard code because this is unclear how to do
+            scale_factors = [2, 4, 8]
+    elif isinstance(image, xarray.DataArray):
+        image = image.image
+
+    # get dtype
+    dtype = type(image.dtype)
+
+    #convert percentiles to quantiles
+    assert lower_percentile < upper_percentile, "Lower percentile must be less than upper percentile"
+    assert lower_percentile >= 0 and upper_percentile <= 100, "Percentiles must be between 0 and 100"
+
+    lower_quantile = lower_percentile / 100
+    upper_quantile = upper_percentile / 100
+
+    # calculate quantiles for specific scale
+    lower_quantiles = image.quantile(lower_quantile, dim = ["x", "y"]).compute().values
+    upper_quantiles = image.quantile(upper_quantile, dim = ["x", "y"]).compute().values
+    IPRs = upper_quantiles - lower_quantiles
+
+    # apply rescaling to image
+    data = image.data
+    data_rescaled = da.map_blocks(lambda x: _rescale_image(x, lower_quantiles, IPRs), data, dtype = dtype)
+
+    # get local transform
+    local_transform = get_transformation(image)
+
+    # write rescaled image back to sdata object
+    write_image(sdata, image = data_rescaled, image_name = f"{image_name}_rescaled", scale_factors = [2, 4, 8], transform = local_transform)
+
+
