002089f24f81275b8a5c895eb783ab350e321498
Sophia Mädler <15019107+sophiamaedler@users.noreply.github.com>
2025-03-23 14:59:34 +0100

    add init files and imports

--- src/scportrait/tools/spdata/__init__.py
+++ src/scportrait/tools/spdata/__init__.py
@@ -1,1 +1,8 @@
 from . import write
+from . import processing
+from . import processing as pp
+
+# has to be done at the end, after everything has been imported
+import sys
+sys.modules.update({f"{__name__}.{m}": globals()[m] for m in ["pp"]})
+
