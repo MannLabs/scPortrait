3f3d1fe765d1fc8c5dd4af4ba4dc3d6e51869867
Sophia Mädler <15019107+sophiamaedler@users.noreply.github.com>
2025-03-26 16:13:58 +0100

    add function to add scalebar to axes

--- src/scportrait/plotting/_utils.py
+++ src/scportrait/plotting/_utils.py
@@ -1,4 +1,12 @@
+from matplotlib.axes import Axes
 from matplotlib.colors import BoundaryNorm, ListedColormap
+
+try:
+    from matplotlib_scalebar.scalebar import ScaleBar
+except ImportError:
+    raise ImportError(
+        "matplotlib_scalebar must be installed to use the plotting capabilites. please install with `pip install 'scportrait[plotting]'`."
+    ) from None
 
 
 def _custom_cmap():
@@ -17,3 +25,19 @@ def _custom_cmap():
     norm = BoundaryNorm(bounds, cmap.N)
 
     return (cmap, norm)
+
+
+def add_scalebar(
+    ax: Axes, resolution: float, resolution_unit: str = "um", location: str = "lower right", color: str = "white"
+) -> None:
+    """Add a scalebar to an axis.
+
+    Args:
+        ax: The axis to add the scalebar to.
+        resolution: The resolution of the image.
+        resolution_unit: The unit of the resolution.
+        location: The location of the scalebar.
+        color: The color of the scalebar.
+    """
+    scalebar = ScaleBar(resolution, resolution_unit, length_fraction=0.2, location=location, frameon=False, color=color)
+    ax.add_artist(scalebar)
