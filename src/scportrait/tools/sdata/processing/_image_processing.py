776774b01e9d7dc423a329e44eb20555059c8460
Sophia Mädler <15019107+sophiamaedler@users.noreply.github.com>
2025-04-03 13:55:39 +0200

    add support for rescaling different channels with different quantiles

--- src/scportrait/tools/sdata/processing/_image_processing.py
+++ src/scportrait/tools/sdata/processing/_image_processing.py
@@ -8,6 +8,8 @@ from spatialdata.transformations import get_transformation
 
 from scportrait.tools.sdata.write._helper import _force_delete_object, rename_image_elem
 from scportrait.tools.sdata.write._write import image as write_image
+from functools import partial
+import spatialdata
 
 ChunkSize2D: TypeAlias = tuple[int, int]
 ChunkSize3D: TypeAlias = tuple[int, int, int]
@@ -33,8 +35,8 @@ def _rescale_image(
 def percentile_normalize_image(
     sdata: SpatialData,
     image_name: str,
-    lower_percentile: float = 0.1,
-    upper_percentile: float = 99.9,
+    lower_percentile: float | list[float] = 0.1,
+    upper_percentile: float | list[float] = 99.9,
     rescaled_image_name: str | None = None,
     scale_factors: list[int] | None = None,
     overwrite: bool = True,
@@ -60,9 +62,12 @@ def percentile_normalize_image(
     if image_name not in sdata:
         raise ValueError(f"Image {image_name} not found in sdata")
 
-    image = sdata[image_name]
+    #define default values for generated rescaled image
     if rescaled_image_name is None:
         rescaled_image_name = f"{image_name}_rescaled"
+
+    # get image and check for proper scaling
+    image = sdata[image_name]
 
     if isinstance(image, xarray.DataTree):
         image = image.get("scale0").image
@@ -75,29 +80,61 @@ def percentile_normalize_image(
 
     # get dtype
     image_dtype = image.data.dtype.type
+        # get channel names
+
+    # get channel names
+    channel_names = image.c.values.tolist()
 
     # convert percentiles to quantiles
-    assert lower_percentile < upper_percentile, "Lower percentile must be less than upper percentile"
-    assert lower_percentile >= 0 and upper_percentile <= 100, "Percentiles must be between 0 and 100"
+    if isinstance(lower_percentile, list) and isinstance(upper_percentile, list):
+        assert len(lower_percentile) == len(upper_percentile), "Lower and upper percentiles must have the same length"
+        for lower, upper in zip(lower_percentile, upper_percentile, strict=True):
+            assert lower < upper, "Lower percentile must be less than upper percentile"
+            assert lower >= 0 and upper <= 100, "Percentiles must be between 0 and 100"
 
-    lower_quantile = lower_percentile / 100
-    upper_quantile = upper_percentile / 100
+        # convert to quantiles
+        lower_quantile = np.array(lower_percentile) / 100
+        upper_quantile = np.array(upper_percentile) / 100
 
-    # calculate quantiles for specific scale
-    lower_quantiles = image.quantile(lower_quantile, dim=["x", "y"]).compute().values
-    upper_quantiles = image.quantile(upper_quantile, dim=["x", "y"]).compute().values
-    IPRs = upper_quantiles - lower_quantiles
+        # calculate quantiles for a specific scale for each channel
+        assert len(image.c) == len(lower_quantile), "Number of channels must match number of quantiles"
+
+        lower_quantiles = np.zeros((len(image.c),), dtype=image_dtype)
+        upper_quantiles = np.zeros((len(image.c),), dtype=image_dtype)
+        IPRs = np.zeros((len(image.c),), dtype=image_dtype)
+
+        for i, x in enumerate(zip(image.c, lower_quantile, upper_quantile, strict = True)):
+            c, lower, upper = x
+            channel = image.sel(c=c)
+            lower_quantiles[i] = channel.quantile(lower, dim=["x", "y"]).compute().values
+            upper_quantiles[i] = channel.quantile(upper, dim=["x", "y"]).compute().values
+            IPRs[i] = upper_quantiles[i] - lower_quantiles[i]
+
+    elif isinstance(lower_percentile, float|int) and isinstance(upper_percentile, float|int):
+        assert lower_percentile < upper_percentile, "Lower percentile must be less than upper percentile"
+        assert lower_percentile >= 0 and upper_percentile <= 100, "Percentiles must be between 0 and 100"
+
+        lower_quantile = lower_percentile / 100
+        upper_quantile = upper_percentile / 100
+
+        # calculate quantiles for specific scale
+        lower_quantiles = image.quantile(lower_quantile, dim=["x", "y"]).compute().values
+        upper_quantiles = image.quantile(upper_quantile, dim=["x", "y"]).compute().values
+        IPRs = upper_quantiles - lower_quantiles
+    else:
+        raise ValueError(f"Lower and upper percentiles are an unsupported dtype {type(lower_percentile)} and {type(upper_percentile)}")
 
     # apply rescaling to image
+    rescale_fn = partial(_rescale_image, lower_quantiles=lower_quantiles, IPRs=IPRs, dtype=image_dtype)
+
     data_rescaled = da.map_blocks(
-        lambda x: _rescale_image(x, lower_quantiles, IPRs, dtype=image_dtype), image.data, dtype=image_dtype
+        rescale_fn,
+        image.data,
+        dtype=image_dtype
     )
 
     # get local transform
     local_transform = get_transformation(image)
-
-    # get channel names
-    channel_names = image.c.values.tolist()
 
     if rescaled_image_name == image_name:
         # to  overwrite the image in place we currently need to use a workaround that
