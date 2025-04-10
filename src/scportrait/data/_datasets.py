d65fcde002ff5cfb8802765c82962ca4f95309b7
Sophia Mädler <15019107+sophiamaedler@users.noreply.github.com>
2025-04-10 19:01:27 +0200

    add additional example config files

--- src/scportrait/data/_datasets.py
+++ src/scportrait/data/_datasets.py
@@ -45,6 +45,9 @@ def get_config_file(config_id, force_download: bool = False) -> Path:
         "dataset_1_wga_config": "https://raw.githubusercontent.com/MannLabs/scPortrait-notebooks/main/example_projects/example_1/config_example1_WGASegmentation.yml",
         "dataset_1_config": "https://raw.githubusercontent.com/MannLabs/scPortrait-notebooks/main/example_projects/example_1/config_example1.yml",
         "dataset_1_custom_cellpost_config": "https://raw.githubusercontent.com/MannLabs/scPortrait-notebooks/main/example_projects/example_1/config_example1_custom_params.yml",
+        "dataset_2_config": "https://raw.githubusercontent.com/MannLabs/scPortrait-notebooks/main/example_projects/example_2/config_example2.yml",
+        "dataset_3_config": "https://raw.githubusercontent.com/MannLabs/scPortrait-notebooks/main/example_projects/example_3/config_example3.yml",
+        "dataset_4_config": "https://raw.githubusercontent.com/MannLabs/scPortrait-notebooks/main/example_projects/example_4/config_example4.yml",
     }
 
     DATASET = config_id
