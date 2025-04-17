[FIX] improve memory overhead for centroid calculation

src/scportrait/pipeline/_utils/segmentation.py
@@ -590,8 +590,9 @@ def numba_mask_centroid(
         >>> centers, sizes, ids = numba_mask_centroid(mask)
     """
     cell_ids = list(np.unique(mask).flatten())
-    if 0 in cell_ids:
-        cell_ids.remove(0)
+    if skip_background:
+        if 0 in cell_ids:
+            cell_ids.remove(0)
     cell_ids = np.array(cell_ids)
 
     min_cell_id = np.min(cell_ids)
@@ -600,7 +601,7 @@ def numba_mask_centroid(
         print("no cells in image. Only contains background with value 0.")
         return None, None, None
 
-    num_classes = int(np.max(mask) if skip_background else np.max(mask) + 1)
+    num_classes = int(len(cell_ids) if skip_background else len(cell_ids) + 1)
 
     points_class = np.zeros((num_classes,), dtype=nb.uint64)
     center = np.zeros((num_classes, 2))
@@ -611,16 +612,18 @@ def numba_mask_centroid(
             for x in range(len(mask[0])):
                 class_id = mask[y, x]
                 if class_id != 0:
-                    points_class[class_id - 1] += 1
-                    center[class_id - 1] += np.array([x, y])
-                    ids[class_id - 1] = class_id
+                    idx = np.where(cell_ids == class_id)[0][0]
+                    points_class[idx] += 1
+                    center[idx] += np.array([x, y])
+                    ids[idx] = class_id
     else:
         for y in range(len(mask)):
             for x in range(len(mask[0])):
                 class_id = mask[y, x]
-                points_class[class_id] += 1
-                center[class_id] += np.array([x, y])
-                ids[class_id] = class_id
+                idx = np.where(cell_ids == class_id)[0][0]
+                points_class[idx] += 1
+                center[idx] += np.array([x, y])
+                ids[idx] = class_id
 
     x = center[:, 0] / points_class
     y = center[:, 1] / points_class